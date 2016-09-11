b94e92b03dba04a7bd23fa0e85542c6a1f1261f3
Maks <spertus07@gmail.com>
2016-09-11 22:29:38 +0200

    Rebuilding app, because of jquery mobile transitions bugs

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,58 +1,64 @@
 ﻿$(document).ready(function () {
     $("#touch_zone_start").click(function () {
-        $.getJSON('http://celtics.spertusiak.com.pl/index.php/Article/GetArticle', function (data) {
-            display_News(data.articles);
-        });
+        get_news();
     });
 });
 
-function display_News(articles) {
-        var div_content = "";
-        for (i = articles.length-1; i >= 0; i--) {//Display from newest to oldest
-            if (i == articles.length - 1) {
-                $("#news_container").html("");
-            }
-            div_content = "<div id='news" + articles[i]['id'] + "' class='news'><p class='news_title'>" + articles[i]['title'] +
-                "</p><div class='mini-img'><img src='http://spertusiak.com.pl/" + articles[i]['small_image_png'] + "' height='150' width='150'/></div>" +
-                articles[i]['content'].substring(0, 200) + "...  <a href='#fullnews" + articles[i]['id'] + "'>Read More</a></div>";
-            $("#news_container").append(div_content);
-            //add news pages with full content
-            remove_news('fullnews' + articles[i]['id']);
-            var news_page = '<div data-role="page" id="fullnews' + articles[i]['id'] + '">' +
-            '<div data-role="header" data-position="fixed" class="main_bar">' +
-                '<a href="#side_menu' + articles[i]['id'] + '" class="ui-btn ui-icon-bars ui-btn-icon-notext ui-nodisc-icon" id="hamburger">Menu</a>' +
-                '<a href="#" class="ui-btn ui-icon-back ui-btn-icon-right ui-btn-icon-notext ui-nodisc-icon" id="back" data-rel="back">back</a>' +
-                '<h1>News</h1>' +
-            '</div>' +
-            '<div role="main" class="ui-content">' +
-            '<div class="full_news_content">' + "<div class='mini-img'><img src='http://spertusiak.com.pl/" + articles[i]['small_image_png'] + "' height='150' width='150'/></div>" + articles[i]['content'] +
-            '</div>' +
-                '<div data-role="panel" id="side_menu' + articles[i]['id'] + '" data-display="overlay" data-position="left">' +
-                    '<ul data-role="listview" data-icon="false">' +
-                        '<li data-role="list-divider">News</li>' +
-                        '<li><a href="#news">Latest news</a></li>' +
-                        '<li><a href="#photos">Photos</a></li>' +
-                        '<li><a href="#video">Video</a></li>' +
-                        '<li data-role="list-divider">Team</li>' +
-                        '<li><a href="#roster">Roster</a></li>' +
-                        '<li><a href="#stats">Stats</a></li>' +
-                        '<li data-role="list-divider">Season</li>' +
-                        '<li><a href="#schedule">Schedule</a></li>' +
-                        '<li><a href="#standings">Standings</a></li>' +
-            '</ul>' +
-                '</div>' +
-            '</div>' +
-            '<div data-role="footer" data-position="fixed" class="main_bar">' +
-                '<h4>Celtics</h4>' +
-            '</div>' +
-        '</div>';
-            $("body").append(news_page);
-        }
+// NEWS FUNCTIONS
+
+function get_news() {
+    $("#back").css('visibility', 'hidden'); //hide news back button
+    $.getJSON('http://celtics.spertusiak.com.pl/index.php/Article/GetArticles', function (data) {
+        display_news(data.articles);
+    });
 }
 
-function remove_news(id) { //function to avoid duplication of news, when reload resources
-    element = document.getElementById(id);
-    if (element) {
-        element.parentNode.removeChild(element);
+function display_news(articles) {
+    var div_content = "";
+    for (i = articles.length - 1; i >= 0; i--) {//Display from newest to oldest
+        if (i == articles.length - 1) {
+            $("#main_container").html("");
+        }
+        div_content = "<div id='news" + articles[i]['id'] + "' class='news'><p class='news_title'>" + articles[i]['title'] +
+            "</p><div class='mini-img'><img src='http://spertusiak.com.pl/" + articles[i]['small_image_png'] + "' height='150' width='150'/></div>" +
+            articles[i]['content'].substring(0, 200) + '...  <a href="#" onclick="read_news(' + articles[i]['id'] + ')">Read More</a></div>';
+        $("#main_container").append(div_content);
+    }
+}
+
+function read_news(id) {
+    $("#main_container").fadeOut(500);
+    var div_content = "";
+    var url = "http://celtics.spertusiak.com.pl/index.php/Article/GetArticle/" + id;
+    setTimeout(function () {
+    $.getJSON(url, function (data) {
+        div_content = '<div class="full_news_content">' + "<div class='mini-img'><img src='http://spertusiak.com.pl/" + data.articles['small_image_png'] +
+            "' height='150' width='150'/></div>" + data.articles['content'] +
+            '</div>';
+        load_news(div_content);
+    });
+    }, 510);
+}
+function load_news(content) {
+    $("#back").css('visibility', 'visible');
+    $("#main_container").html('');
+    $("#main_container").html(content);
+    $(window).scrollTop(0);
+    $("#main_container").fadeIn(500);
+}
+
+//PHOTOS FUNCTIONS
+
+function get_gallery() {
+    $.getJSON('http://celtics.spertusiak.com.pl/index.php/Photos/GetPhotos', function (data) {
+        display_gallery(data.photos);
+    });
+}
+
+function display_gallery(photos) {
+    for (i = 0; i < photos.length; i++) {
+        var content = '<a href="http://celtics.spertusiak.com.pl/gallery/' + photos[i]['big'] +
+            '" class="swipebox" title="My Caption"><img src="http://celtics.spertusiak.com.pl/gallery/' + photos[i]['small'] + '" alt="' + photos[i]['name'] + '"></a>'
+        $("#photos_container").append(content);
     }
 }
